d311a2f9c27cc9efe81ca09b8d3249102556aec1
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2023-12-14 09:42:24 +0100

    Add Welcome message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Willkommen zu meinem Shop");
+        System.out.println("Was wollen Sie bestellen?");
     }
 }
